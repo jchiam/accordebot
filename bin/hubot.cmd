080007f26119fa5c4a318f985e1886949f6b7900
Jonathan Chiam <dcsczj@nus.edu.sg>
2017-02-14 14:54:09 +0800

    Add debug logging

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,5 +3,6 @@
 call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+SET HUBOT_LOG_LEVEL=debug
 
-node_modules\.bin\hubot.cmd --name "accordebot" %* 
+node_modules\.bin\hubot.cmd --name "accordebot" %*
