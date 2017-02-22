fc13a8da458d7685865af306880900e7b6560671
Jonathan Chiam <dcsczj@nus.edu.sg>
2017-02-22 18:10:55 +0800

    Fix role errors

--- scripts/utils/roles.js
+++ scripts/utils/roles.js
@@ -1,4 +1,5 @@
 const firebase = require('firebase');
+const userUtils = require('./users');
 
 const ADMIN = 'admin';
 const GUARDIAN = 'guardian';
@@ -13,6 +14,38 @@ const getRoles = (callback) => {
   });
 };
 
+const getUserRoles = (key, callback) => {
+  const userRoles = [];
+  const name = userUtils.getKeyNameFromKey(key);
+
+  firebase.database().ref('/roles').on('value', (snapshot) => {
+    if (snapshot.exists()) {
+      const roles = snapshot.val();
+      for (const role in roles) {
+        if (roles[role].includes(name)) {
+          userRoles.push(role);
+        }
+      }
+      callback(null, userRoles);
+    } else {
+      callback(new Error('Failed to retrieve roles...'));
+    }
+  });
+};
+
+const isUserRole = (key, role, callback) => {
+  const name = userUtils.getKeyNameFromKey(key);
+  firebase.database().ref(`/roles/${role}`).on('value', (snapshot) => {
+    if (snapshot.exists()) {
+      callback(null, snapshot.val().includes(name));
+    } else {
+      callback(new Error(`Invalid role ${role} supplied...`));
+    }
+  });
+};
+
 exports.ADMIN = ADMIN;
 exports.GUARDIAN = GUARDIAN;
 exports.getRoles = getRoles;
+exports.getUserRoles = getUserRoles;
+exports.isUserRole = isUserRole;
